f4cb12f2282360c86b92d7f9572e0c5a2bb115f1
cbittgc <62404903+cbittgc@users.noreply.github.com>
2022-08-12 18:55:48 -0300

    Add files via upload

---
+++ aaaaaaaaaaa.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+double f(double varA, double varB, double varC, double varD, double x) {
+    return varA*pow(x,3)+varB*pow(x,2)+varC*x+varD;
+}
+
+
+int main() {
+    double varA,varB,varC,varD;
+    double limA, limB;
+
+    printf("Digite o valor de A: ");
+    scanf("%lf", &varA);
+    printf("Digite o valor de B: ");
+    scanf("%lf", &varB);
+    printf("Digite o valor de C: ");
+    scanf("%lf", &varC);
+    printf("Digite o valor de D: ");
+    scanf("%lf", &varD);
+    printf("Digite o valor de limite esquerdo: ");
+    scanf("%lf", &limA);
+    printf("Digite o valor de limite direito: ");
+    scanf("%lf", &limB);
+    
+    double resLimA = f(varA, varB, varC, varD, limA);
+    double resLimB = f(varA, varB, varC, varD, limB);
+    double b = resLimB - resLimA;
+    double h = (limA-limB);
+    double areaN = b*h;
+    h = h/2;
+    resLimA = f(varA, varB, varC, varD, limA);
+    resLimB = f(varA, varB, varC, varD, h);
+    double areaN1 = b*h;
+
+    while(abs(areaN-areaN1) < 0.000001) {
+        areaN = areaN1;
+        h = h/2;
+        resLimA = f(varA, varB, varC, varD, limA);
+        resLimB = f(varA, varB, varC, varD, h);
+        areaN1 = b*h;
+        
+    }
+
+    printf("A area do trapézio é: %lf\n", areaN1);
+
+    return 0;
+}
