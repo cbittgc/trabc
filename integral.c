9ee9d1caf68a7e54338a7563cf478259e8253187
cbittgc <62404903+cbittgc@users.noreply.github.com>
2022-08-12 19:57:45 -0300

    Update and rename aaaaaaaaaaa.c to integral.c

--- integral.c
+++ integral.c
@@ -11,29 +11,26 @@ double f(double x) {
 double area(double limA, double limB) {
     double resLimA = f(limA);
     double resLimB = f(limB);
-    double h = resLimB - resLimA;
+    double h = resLimA;
     double b = fabs((limA-limB));
     return b*h;
 }
 
 double integral(double limA, double limB) {
     double b = fabs((limA-limB));
-    double areaAnterior = -1;
-    double areaAtual;
-    do {
-        printf("rodou");
+    double areaAnterior = -999999999;
+    double areaAtual = -999999999;
+    do { 
         areaAnterior = areaAtual;
         areaAtual = 0;
         for(double x = limA; x < limB; x+=b){
             areaAtual += area(x, x+b);
         }
         b /= 2;
-        printf("aAt: %lf\naAn: %lf\nfabs: %lf\n",areaAtual,areaAnterior,fabs(areaAnterior - areaAtual));
     } while(fabs(areaAnterior - areaAtual) > 0.000001);
     
     return areaAtual;
 }
-
 
 int main() {
     double limA, limB;
@@ -51,7 +48,7 @@ int main() {
     printf("Digite o valor de limite direito: ");
     scanf("%lf", &limB);
     
-    printf("%lf",integral(limA, limB));
+    printf("\n%lf\n",integral(limA, limB));
 
     return 0;
 }
